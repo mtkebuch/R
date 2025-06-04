1274603f55f2723af7d9b4fdcfead83ed7593f49
Mariami <mtkebuchava222@gmail.com>
2025-06-04 18:45:54 +0400

    ,

--- mtcars.R
+++ mtcars.R
@@ -28,3 +28,32 @@ max
 max_1=max(df[df$vs!=1 & df$am!=0,]$mpg)
 max_1
 
+#1
+qsec=mtcars$qsec
+br=c(14.5,17,21,23)
+labels=c('A','B','C')
+mtcars$cat=cut(qsec,breaks=br,labels=labels,right=FALSE)
+mtcars$cat
+
+cyl4=mean(mtcars[mtcars$cyl==4,]$hp)
+cyl8=mean(mtcars[mtcars$cyl==8,]$hp)
+
+cyl8-cyl4
+
+#2
+a_cat=mean(mtcars$wt[mtcars$cat=='A' & mtcars$hp<230])
+a_cat
+
+#3
+a_disp=mean(mtcars$hp[mtcars$cat=='A' & mtcars$disp>240])
+a_disp
+
+#4
+b_disp=min(mtcars$mpg[(mtcars$cat=='B' & mtcars$disp>140) & mtcars$gear>4])
+b_disp
+
+#5
+all=nrow(mtcars[mtcars$cat=='A',])
+part=sum(mtcars[mtcars$cat=='A',]$gear==3)
+part/all*100
+
